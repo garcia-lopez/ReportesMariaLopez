dd5ac00dc140b49aa2135103cacd2c81488d6b3a
garcia-lopez <maria.lopez14959@est.uca.edu.ni>
2023-05-24 15:28:51 -0600

    Diseño Reporte

---
+++ Reportes/Reportes/Reporte/Reporte/Form1.Designer.vb
@@ -0,0 +1,52 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BtnImprimirCargo = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'BtnImprimirCargo
+        '
+        Me.BtnImprimirCargo.BackColor = System.Drawing.Color.Lavender
+        Me.BtnImprimirCargo.Location = New System.Drawing.Point(49, 57)
+        Me.BtnImprimirCargo.Name = "BtnImprimirCargo"
+        Me.BtnImprimirCargo.Size = New System.Drawing.Size(129, 47)
+        Me.BtnImprimirCargo.TabIndex = 0
+        Me.BtnImprimirCargo.Text = "Mostrar Reporte de Cargo"
+        Me.BtnImprimirCargo.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage
+        Me.BtnImprimirCargo.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(224, 164)
+        Me.Controls.Add(Me.BtnImprimirCargo)
+        Me.Name = "Form1"
+        Me.Text = "Principal"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents BtnImprimirCargo As Button
+End Class
---
+++ Reportes/Reportes/Reporte/Reporte/Form1.vb
@@ -0,0 +1,36 @@
+﻿
+
+Imports System.Data.SqlClient
+Imports Microsoft.Reporting.WinForms
+
+Public Class Form1
+    Sub verReporte(ByVal t As DataTable, ByVal ds As String, ByVal nombreRpt As String)
+        Try
+            Dim rpt As New ReportDataSource(ds, t)
+
+            FrmVistaPrevia.ReportViewer1.LocalReport.DataSources.Clear()
+            FrmVistaPrevia.ReportViewer1.LocalReport.DataSources.Add(rpt)
+            FrmVistaPrevia.ReportViewer1.LocalReport.ReportPath = nombreRpt
+            FrmVistaPrevia.ReportViewer1.Refresh()
+            FrmVistaPrevia.Show()
+
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical, "Error al mostrar reporte")
+
+        End Try
+    End Sub
+
+    Private Sub BtnImprimirCargo_Click(sender As Object, e As EventArgs) Handles BtnImprimirCargo.Click
+        Try
+            Dim tsql As String = "Select * from jobs"
+            Dim conex As New SqlConnection(My.Settings.cStrDBRH)
+            Dim ds As New SqlDataAdapter(tsql, conex)
+            Dim t As New DataTable
+            ds.Fill(t)
+            verReporte(t, "dsRegistros", "diseñosRpt\rptCargo.rdlc")
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical, "Error al cargar reporte")
+
+        End Try
+    End Sub
+End Class
