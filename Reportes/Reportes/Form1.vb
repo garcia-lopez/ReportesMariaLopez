8e3a674b1e0e6a09cc6c2c084bc0305920656982
garcia-lopez <maria.lopez14959@est.uca.edu.ni>
2023-05-24 16:07:59 -0600

    trabajo de clase

--- Reportes/Reportes/Form1.vb
+++ Reportes/Reportes/Form1.vb
@@ -1,3 +1,35 @@
-﻿Public Class Form1
+﻿Imports System.Data.SqlClient
+Imports Microsoft.Reporting.WinForms
 
+
+
+Public Class Form1
+
+    Sub verReporte(ByVal t As DataTable, ByVal ds As String, ByVal nombreRpt As String)
+        Try
+            Dim rpt As New ReportDataSource(ds, t)
+
+            FrmVistaPrevia.ReportViewer1.LocalReport.DataSources.Clear()
+            FrmVistaPrevia.ReportViewer1.LocalReport.DataSources.Add(rpt)
+            FrmVistaPrevia.ReportViewer1.LocalReport.ReportPath = nombreRpt
+            FrmVistaPrevia.ReportViewer1.Refresh()
+            FrmVistaPrevia.Show()
+
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical, "Error al mostrar reporte")
+        End Try
+    End Sub
+
+    Private Sub BtnImprimirCargos_Click_1(sender As Object, e As EventArgs) Handles BtnImprimirCargos.Click
+        Try
+            Dim tsql As String = "Select * from jobs"
+            Dim conex As New SqlConnection(My.Settings.strConn)
+            Dim da As New SqlDataAdapter(tsql, conex)
+            Dim t As New DataTable
+            da.Fill(t)
+            verReporte(t, "dsRegistros", "diseñosRpt\rptCargo.rdlc")
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical, "Error al cargar reporte")
+        End Try
+    End Sub
 End Class
