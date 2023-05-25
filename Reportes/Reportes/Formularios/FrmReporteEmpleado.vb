c7e4b0668ed9c70ea1127c56d599ecee2d56dfab
garcia-lopez <maria.lopez14959@est.uca.edu.ni>
2023-05-25 11:14:59 -0600

    Se inició la primera guia

--- Reportes/Reportes/Formularios/FrmReporteEmpleado.vb
+++ Reportes/Reportes/Formularios/FrmReporteEmpleado.vb
@@ -1,4 +1,13 @@
-﻿
+﻿Imports Microsoft.Reporting.WinForms
 Public Class FrmReporteEmpleado
+
+    Dim tblEmp As New BdHRDataSet.RptJobEmployeesDataTable
+    Dim rptEmpleados As New BdHRDataSetTableAdapters.RptJobEmployeesTableAdapter
+    Dim tbl As New DataTable
+
+    Sub LlenarGrid()
+        rptEmpleados.Fill(tblEmp)
+        DgvRe
+    End Sub
 
 End Class
